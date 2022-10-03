9792df12ae5e27353b2bb29daa019a976905d74c
RoofMi <99576694+RoofMi@users.noreply.github.com>
2022-10-03 10:52:31 +0900

    [Bronze V] Title: 사분면 고르기, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -7,19 +7,19 @@ public class Main {
 		int x = scan.nextInt();
 		int y = scan.nextInt();
 		
-		if ((x >= -1000 && x <= 1000 && x != 0) && (y >= -1000 && y <= 1000 && y != 0))
-		if (x > 0 && y > 0) {
-			System.out.println("1");
-		}
-		else if (x < 0 && y > 0){
-			System.out.println("2");
-		}
-		else if (x < 0 && y < 0){
-			System.out.println("3");
-		}
-		else {
-			System.out.println("4");
-		}
-		
+		if ((x >= -1000 && x <= 1000 && x != 0) && (y >= -1000 && y <= 1000 && y != 0)) {
+            if (x > 0 && y > 0) {
+			    System.out.println("1");
+		    }
+		    else if (x < 0 && y > 0){
+			    System.out.println("2");
+		    }
+		    else if (x < 0 && y < 0){
+			    System.out.println("3");
+		    }
+		    else {
+			    System.out.println("4");
+		    }
+        }
 	}
 }
