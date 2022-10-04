9edae4cd0a99b58a95fbf3eba47ccd959cdc9ea6
RoofMi <99576694+RoofMi@users.noreply.github.com>
2022-10-04 12:48:52 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 356 ms, Memory: 18364 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		if (n >= 1 && n <= 100) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 1; j < n-i; j++) {
+					System.out.print(" ");
+				}
+				for (int k = 0; k < i+1; k++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+    }	
+}
