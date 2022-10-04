cd5ef709a9c543983faa7d5bf6d2b701d7f2306f
RoofMi <99576694+RoofMi@users.noreply.github.com>
2022-10-04 10:09:40 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -2,18 +2,17 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        int n = scan.nextInt();
-        
-        for (int i = 0; i < n; i++) {
-        	int a = scan.nextInt();
-            int b = scan.nextInt();
-            
-            if (a > 0 && b < 10) {
-                System.out.println(a+b);
-            }
-        }
-        
+		Scanner scan = new Scanner(System.in);
+		
+		int t = scan.nextInt();
+		
+		for (int i = 0; i < t; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			
+			if(a > 0 && b < 10) {
+				System.out.println(a+b);
+			}
+		}
     }
 }
