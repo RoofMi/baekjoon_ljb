1a95abe14dda56167ce6973e08323d9ab98bf557
RoofMi <99576694+RoofMi@users.noreply.github.com>
2022-10-06 19:08:04 +0900

    [Bronze V] Title: A+B - 4, Time: 232 ms, Memory: 17904 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,17 +1,19 @@
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
-        while(scan.hasNextInt()) {
-        	int a = scan.nextInt();
-            int b = scan.nextInt();
-            
-            if (a > 0 && b < 10) {
-                System.out.println(a+b);
-            }
-        }
-    
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if (a > 0 && b < 10) {
+				System.out.println(a + b);
+			}
+		}
+
+	}
+
 }
