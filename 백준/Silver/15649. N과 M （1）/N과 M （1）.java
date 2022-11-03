f601580006141ffc8be4e095072ffb4c3721aa3e
RoofMi <99576694+RoofMi@users.noreply.github.com>
2022-11-03 10:52:38 +0900

    [Silver III] Title: N과 M (1), Time: 2644 ms, Memory: 79540 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int n1;
+	static int n2;
+	static int []list;
+	static boolean []visited;
+	static int []subset;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		n1 = Integer.parseInt(st.nextToken());
+		n2 = Integer.parseInt(st.nextToken());
+		
+		list = new int[n1];
+		visited = new boolean[n1];
+		subset = new int[n2];
+		
+		for (int i = 0; i < n1; i++) {
+			list[i] = i+1;
+			visited[i] = false;
+		}
+		
+		com(0, n2);
+	}
+	
+	public static void com(int depth, int r) {		
+		if (depth == r) {
+			for (int i = 0; i < r; i++) {
+				System.out.print(subset[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for (int i = 0; i < n1; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				subset[depth] = list[i];
+				com(depth+1, r);
+				visited[i] = false;
+			}
+		}
+	}
+}
