e33f54bc004586c59f3f8ca05d9435fc207218bc
RoofMi <99576694+RoofMi@users.noreply.github.com>
2022-11-06 12:38:28 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 16720 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int n1;
+	static int n2;
+	static int []list;
+	static int []subset;
+	static StringBuilder sb = new StringBuilder();;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		
+		n1 = Integer.parseInt(st.nextToken());
+		n2 = Integer.parseInt(st.nextToken());
+		
+		list = new int[n1];
+		subset = new int[n2];
+		
+		for (int i = 0; i < n1; i++) {
+			list[i] = i+1;
+		}
+		
+		com(0, 0, n2);
+		System.out.println(sb);
+	}
+	
+	public static void com(int depth, int start, int r) {		
+		if (depth == r) {
+			for (int i = 0; i < r; i++) {
+				sb.append(subset[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = start; i < n1; i++) {
+			subset[depth] = list[i];
+			com(depth+1, i, r);
+		}
+	}
+}
