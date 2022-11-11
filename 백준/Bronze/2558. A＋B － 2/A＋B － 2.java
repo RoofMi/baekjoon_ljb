d43ace4d80afc0d16ae779e120ebe7101b0d74eb
RoofMi <99576694+RoofMi@users.noreply.github.com>
2022-11-11 18:06:36 +0900

    [Bronze V] Title: A+B - 2, Time: 212 ms, Memory: 17788 KB -BaekjoonHub

--- 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -1,15 +1,12 @@
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
         
-        int a = scan.nextInt();
-        scan.nextLine();
-        int b = scan.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         
-        if (a > 0 && b < 10) {
-            System.out.println(a+b);
-        }
+        System.out.println(a+b);
     }
 }
