04d5c671efb0b25591d735f8f59c63cacba9ee1b
ZeNiff <leejbsocial@gmail.com>
2024-11-10 15:48:58 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 208 ms, Memory: 10796 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> list;
+vector<int> visitedOrder;
+int order = 1;
+
+void dfs(int start)
+{
+	visitedOrder[start] = order;
+
+	for (int i = 0; i < list[start].size(); i++)
+	{
+		int next = list[start].at(i);
+
+		if (!visitedOrder[next])
+		{
+			order++;
+			dfs(next);
+		}	
+	}
+}
+
+int main()
+{
+	int N, M, R = 0;
+
+	cin >> N >> M >> R;
+
+	list = vector<vector<int>>(N + 1);
+	visitedOrder = vector<int>(N + 1, 0);
+
+	for (int i = 0; i < M; i++)
+	{
+		int u, v;
+
+		cin >> u >> v;
+
+		list[u].push_back(v);
+		list[v].push_back(u);
+	}
+
+	for (int i = 0; i < N; i++)
+		sort(list[i].begin(), list[i].end());
+	
+	dfs(R);
+
+	for (int i = 1; i < N + 1; i++)
+		cout << visitedOrder[i] << "\n";
+
+	return 0;
+}
