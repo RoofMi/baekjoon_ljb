154cf0cb07acbf358a6a907cf9c7aaead10a741e
ZeNiff <leejbsocial@gmail.com>
2024-11-20 13:49:41 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int list[100001] = { 0, };
+	int N, K;
+	cin >> N >> K;
+
+	int idx = K;
+
+	vector<int> ans;
+
+	for (int i = 1; i <= N; i++)
+		list[i] = i;
+
+	while (true)
+	{
+		if (list[idx] != 0)
+		{
+			ans.push_back(idx);
+			list[idx] = 0;
+		}
+
+		if (ans.size() >= N)
+			break;
+		else
+		{
+			int move = 0;
+			while (move < K)
+			{
+				if (idx + 1 > N)
+					idx = 1;
+				else
+					idx += 1;
+
+				if (list[idx] != 0)
+					move++;
+			}
+		}
+	}
+
+	cout << '<';
+	for (int i = 0; i < ans.size() - 1; i++)
+		cout << ans.at(i) << ", ";
+	cout << ans.back() << ">\n";
+
+	return 0;
+}
