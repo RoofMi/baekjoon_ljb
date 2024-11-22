97282a289a421bf82c20c229eeba43b5f22edde3
ZeNiff <leejbsocial@gmail.com>
2024-11-22 13:42:51 +0900

    [Silver IV] Title: 듣보잡, Time: 40 ms, Memory: 7572 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(0);
+
+	int N, M;
+	set <string> list;
+	set <string> ans;
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		string temp;
+		cin >> temp;
+
+		list.insert(temp);
+	}
+
+	for (int i = 0; i < M; i++)
+	{
+		string temp;
+		cin >> temp;
+
+		auto check = list.find(temp);
+		if (check != list.end())
+			ans.insert(temp);
+	}
+
+	cout << ans.size() << '\n';
+	for (string str : ans)
+		cout << str << '\n';
+
+	return 0;
+}
