d60e39ab55ca66c42618fb02d1309a7899623847
ZeNiff <leejbsocial@gmail.com>
2025-01-18 12:29:26 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -4,18 +4,18 @@ using namespace std;
  
 int main()
 {
-	int N;
+	int N, temp;
     int M = 0, Y = 0;
 	cin >> N;
  
 	for (int i = 0; i < N; i++)
     {
-        int temp;
 		cin >> temp;
         
 		Y += (temp / 30 + 1) * 10;
 		M += (temp / 60 + 1) * 15;
 	}
+    
 	if (M < Y)
         cout << "M " << M;
 	else if (M > Y)
